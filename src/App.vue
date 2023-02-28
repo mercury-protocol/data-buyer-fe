0dc3eda7e389a1b5dec3957c4f8a657dba23eaed
Marco Germanier <marco@mrgt.me>
2023-02-28 13:09:17 -0700

    config: added cypherwallet

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,23 @@
 
 <script>
 import Home from "./views/Home.vue"
+import { createScript } from './utils.js' // import a utility function to create script element
+
+
 export default {
   name: 'App',
   components: {
     Home,
-  }
+  },
+  mounted() {
+    createScript('https://public.cypherd.io/js/onboardingsdk.jss', 'onboardingsdk').then(() => {
+      // The onboardingsdk script has been loaded
+      // You can now use the onboardingsdk global variable
+    }).catch((error) => {
+      // There was an error loading the onboardingsdk script
+      console.error(error)
+    });
+  },
 }
 </script>
+
---
+++ src/utils.js
@@ -0,0 +1,11 @@
+export function createScript(src, id) {
+  return new Promise((resolve, reject) => {
+    const script = document.createElement("script");
+    script.src = src;
+    script.id = id;
+    script.async = true;
+    script.onload = () => resolve();
+    script.onerror = (error) => reject(error);
+    document.head.appendChild(script);
+  });
+}
