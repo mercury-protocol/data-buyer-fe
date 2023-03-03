9dd456d07e2f15a9e0cd889bef18765b57064d42
Marco Germanier <marco@mrgt.me>
2023-03-03 14:46:14 -0700

    Revert "feat: broken walletconnect"
    This reverts commit 6e342d31035d8fbfe9209d55294dda096aa90b4e.

--- src/components/ConnectButton.vue
+++ src/components/ConnectButton.vue
@@ -1,82 +1,57 @@
 <template>
-    <Button @click="onButtonClick" :label="connectButtonLabel"></Button>
+    <div class="w-1/2 pr-8 items-end">
+        <Button class="float-right p-button-outlined" :disabled="disabled" @click="connect" :label="buttonText" />
+    </div>
 </template>
-
+  
 <script>
-
-// import ethereumClient from '../services/wagmi'
-import { Web3Modal } from '@web3modal/html'
-import { configureChains, createClient } from "@wagmi/core";
-import { goerli, filecoinHyperspace } from "@wagmi/core/chains";
-import {
-    EthereumClient,
-    modalConnectors,
-    walletConnectProvider,
-} from "@web3modal/ethereum";
-
+import { SignClient } from '@walletconnect/sign-client'
+import { Web3Modal } from '@web3modal/standalone'
 
 export default {
     data() {
         return {
-            web3Modal: {},
-            connectButtonLabel: "Connect Wallet",
-            chains: [goerli, filecoinHyperspace],
-            projectId: "67d1f7f27d715a4ffeb49e489586cc32"
+            disabled: true,
+            buttonText: 'Initializing...',
+            projectId: '67d1f7f27d715a4ffeb49e489586cc32',
+            namespaces: {
+                eip155: { methods: ['eth_sign'], chains: ['eip155:5'], events: ['accountsChanged'] }
+            },
+            web3Modal: null,
+            signClient: null
         }
     },
-    created() {
-        const chains = this.chains;
-        const projectId = this.projectId;
-        const { provider } = configureChains(chains, [
-            walletConnectProvider({ projectId }),
-        ]);
-        const wagmiClient = createClient({
-            autoConnect: true,
-            connectors: [
-                ...modalConnectors({
-                    appName: "web3Modal",
-                    chains,
-                    version: "2",
-                    projectId: projectId,
-                }),
-                chains,
-                // new SafeConnector({ chains }),
-            ],
-            provider,
+    async created() {
+        this.web3Modal = new Web3Modal({ projectId: this.projectId, standaloneChains: this.namespaces.eip155.chains })
+        try {
+            this.signClient = await SignClient.init({ projectId: this.projectId })
+            this.disabled = false
+            this.buttonText = 'Connect Wallet'
+        } catch (err) {
+            console.error(err)
+        }
+        this.web3modal.setTheme({
+            themeMode: "dark",
+            themeColor: "blue",
+            themeBackground: "themeColor",
         });
-        const ethereumClient = new EthereumClient(wagmiClient, chains);
-        console.log('ethereumClient', ethereumClient)
-        console.log('CREATED')
-        this.web3Modal = new Web3Modal(
-            {
-                projectId: '67d1f7f27d715a4ffeb49e489586cc32',
-                // walletImages: {
-                //     // goerli: '',
-                //     filecoinHyperspace: 'https://asset.brandfetch.io/id53RKKEgl/idrM5KW9wi.png'
-                // }
-            },
-            ethereumClient
-        )
-        console.log('this.web3Modal', this.web3Modal)
     },
     methods: {
-        async onButtonClick() {
-            console.log('W3M Core Button clicked');
-            await this.web3Modal.openModal();
-            // this.onAccountUpdate();
-        },
-        // onAccountUpdate() {
-        //     console.log('GET ACCOUNT')
-        //     const account = getAccount()
-        //     console.log(account);
-
-
-        // const { data: ensName } = useEnsName({ address })
-        // if (isConnected) {
-        //     this.connectButtonLabel = ensName ? ensName : address;
-        // }
-
-        // }
+        async connect() {
+            try {
+                if (this.signClient) {
+                    const { uri, approval } = await this.signClient.connect({ requiredNamespaces: this.namespaces })
+                    if (uri) {
+                        await this.web3Modal.openModal({ uri })
+                        await approval()
+                        this.web3Modal.closeModal()
+                    }
+                }
+            } catch (err) {
+                console.error(err)
+            }
+        }
     }
 }
 </script>
+  
