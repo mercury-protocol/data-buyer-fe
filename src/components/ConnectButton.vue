b9a2a28fe1e7540d7f1499d266baff51085a7e14
Marco Germanier <marco@mrgt.me>
2023-03-04 11:39:08 -0700

    fix: walletconnect still broken

--- src/components/ConnectButton.vue
+++ src/components/ConnectButton.vue
@@ -1,57 +1,40 @@
+// ConnectButton.vue
 <template>
-    <div class="w-1/2 pr-8 items-end">
-        <Button class="float-right p-button-outlined" :disabled="disabled" @click="connect" :label="buttonText" />
-    </div>
+    <Button @click="onButtonClick">W3M Core Button</Button>
 </template>
-  
+
 <script>
-import { SignClient } from '@walletconnect/sign-client'
-import { Web3Modal } from '@web3modal/standalone'
+import ethereumClient from '@/services/walletconnect'
+import { Web3Modal } from '@web3modal/html'
 
 export default {
     data() {
         return {
-            disabled: true,
-            buttonText: 'Initializing...',
-            projectId: '67d1f7f27d715a4ffeb49e489586cc32',
-            namespaces: {
-                eip155: { methods: ['eth_sign'], chains: ['eip155:5'], events: ['accountsChanged'] }
-            },
-            web3Modal: null,
-            signClient: null
+            web3Modal: undefined
         }
     },
-    async created() {
-        this.web3Modal = new Web3Modal({ projectId: this.projectId, standaloneChains: this.namespaces.eip155.chains })
-        try {
-            this.signClient = await SignClient.init({ projectId: this.projectId })
-            this.disabled = false
-            this.buttonText = 'Connect Wallet'
-        } catch (err) {
-            console.error(err)
-        }
-        this.web3modal.setTheme({
-            themeMode: "dark",
-            themeColor: "blue",
-            themeBackground: "themeColor",
-        });
+    created() {
+        console.log(ethereumClient);
+        setTimeout(() => {
+            this.web3Modal = new Web3Modal(
+                {
+                    projectId: '67d1f7f27d715a4ffeb49e489586cc32',
+                    walletImages: {}
+                },
+                ethereumClient
+            )
+
+            console.log(this.web3Modal);
+
+        }, 30000)
+
+
     },
     methods: {
-        async connect() {
-            try {
-                if (this.signClient) {
-                    const { uri, approval } = await this.signClient.connect({ requiredNamespaces: this.namespaces })
-                    if (uri) {
-                        await this.web3Modal.openModal({ uri })
-                        await approval()
-                        this.web3Modal.closeModal()
-                    }
-                }
-            } catch (err) {
-                console.error(err)
-            }
+        async onButtonClick() {
+            console.log('W3M Core Button clicked');
+            await this.web3Modal.openModal();
         }
     }
 }
 </script>
-  
